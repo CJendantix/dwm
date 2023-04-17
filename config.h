a46d40b15fa599ec5e5b50fe93a51756e8d769ed
CJ Gilroy <cgilroy33@gmail.com>
2023-04-17 15:44:52 -0400

    fixed screenshot names

--- config.h
+++ config.h
@@ -74,7 +74,7 @@ static const char *mutecmd[] = { "amixer", "-q", "set", "Master", "toggle", NULL
 static const char *volupcmd[] = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
 static const char *voldowncmd[] = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
 static const char *miccmd[] = { "amixer", "set", "Capture", "toggle", NULL };
-static const char *screenshot[] = { "scrot", "-u", "\"$(date)\".jpg", NULL };
+static const char *screenshot[] = { "scrot", "-u", "screenshot.jpg", NULL };
 
 #include <X11/XF86keysym.h>
 #include "shiftview.c"
